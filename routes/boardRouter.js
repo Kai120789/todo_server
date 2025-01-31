ddd207b1d43609ad2647179463b71f03a1bb215d
Kai120789 <zoomer120789@gmail.com>
2025-01-31 22:56:00 +0300

    feat: added middleware to task and board routers

--- routes/boardRouter.js
+++ routes/boardRouter.js
@@ -2,12 +2,12 @@ const Router = require("express")
 const router = new Router()
 const boardsController = require("../controllers/boardController")
 
-router.post('/', boardsController.create) // create board
-router.put('/:id', boardsController.update) // update board
-router.get('/', boardsController.getAll) // get all boards
-router.get('/:id', boardsController.getOne) // get board by id
-router.delete('/:id', boardsController.delete) // delete board by id
+router.post('/', authMiddleware, boardsController.create) // create board
+router.put('/:id', authMiddleware, boardsController.update) // update board
+router.get('/', authMiddleware, boardsController.getAll) // get all boards
+router.get('/:id', authMiddleware, boardsController.getOne) // get board by id
+router.delete('/:id', authMiddleware, boardsController.delete) // delete board by id
 
-router.post('/:boardId', boardsController.addUserToBoard) // add user to board
+router.post('/:boardId', authMiddleware, boardsController.addUserToBoard) // add user to board
 
 module.exports = router
--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -2,10 +2,10 @@ const Router = require("express")
 const router = new Router()
 const taskController = require("../controllers/taskController")
 
-router.post('/', taskController.create) // create task
-router.put('/:id', taskController.update) // update task
-router.get('/', taskController.getAll) // get all tasks
-router.get('/:id', taskController.getOne) // get task by id
-router.delete('/:id', taskController.delete) // delete task by id
+router.post('/', authMiddleware, taskController.create) // create task
+router.put('/:id', authMiddleware, taskController.update) // update task
+router.get('/', authMiddleware, taskController.getAll) // get all tasks
+router.get('/:id', authMiddleware, taskController.getOne) // get task by id
+router.delete('/:id', authMiddleware, taskController.delete) // delete task by id
 
 module.exports = router
